26a05cbc968a8429e50a2a0f30e56404ab00ae33
aa_maslov <smart.maslov@gmail.com>
2022-02-15 11:50:30 +0300

    V3

--- src/test/java/help/DBHelper.java
+++ src/test/java/help/DBHelper.java
@@ -6,32 +6,71 @@ import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
 import org.apache.commons.dbutils.handlers.ScalarHandler;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.List;
+import java.util.Properties;
 
 public class DBHelper {
-    private DBHelper() {}
-
-    private static final QueryRunner runner = new QueryRunner();
-    private static final Connection conn = connect();
-
-    @SneakyThrows
-    public static Connection connect() {
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/app-db", "app", "pass");
+    private DBHelper() {
     }
 
-    @SneakyThrows
-    public static String getCode() {
-        return runner.query(conn, "SELECT DISTINCT LAST_VALUE(code) over (ORDER BY created ASC\n" +
-                "    RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) FROM auth_codes", new ScalarHandler<>());
-    }
+    private final QueryRunner runner = new QueryRunner();
+    private Properties prop = prop();
+    private final Connection conn = getConnect();
 
+    private Properties prop() {
+        Properties properties = new Properties();
+        try (InputStream is = DBHelper.class.getClassLoader().getResourceAsStream("application.properties")) {
+            properties.load(is);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return properties;
+    }
     @SneakyThrows
-    public static void deleteCode() {
-        runner.execute(conn, "DELETE FROM auth_codes WHERE created < NOW() - INTERVAL 0 MINUTE");
-        runner.execute(conn, "DELETE FROM card_transactions WHERE created < NOW() - INTERVAL 0 MINUTE");
-        runner.execute(conn, "DELETE FROM cards WHERE cards.number IS NOT NULL");
-        runner.execute(conn, "DELETE FROM users WHERE login IS NOT NULL");
+    private Connection getConnect() {
+        return DriverManager.getConnection(
+                prop.getProperty("spring.datasource.url"),
+                prop.getProperty("spring.datasource.username"),
+                prop.getProperty("spring.datasource.password")
+        );
+    }
+    public static class DeleteInfo {
+        @SneakyThrows
+        public static void deletingData() {
+            var deleteFromPaymentEntity = "DELETE FROM payment_entity;";
+            var deleteFromCreditRequestEntity = "DELETE FROM credit_request_entity;";
+            var deleteFromOrderEntity = "DELETE FROM order_entity;";
+
+            try (
+                    var conn = DriverManager.getConnection(
+                            "jdbc:mysql://localhost:3306/aqa-shop", "aqa", "admin"
+                    );
+                    var deleteStmt = conn.createStatement();
+            ) {
+
+                var paymentEntity = deleteStmt.executeUpdate(deleteFromPaymentEntity);
+                var creditRequestEntity = deleteStmt.executeUpdate(deleteFromCreditRequestEntity);
+                var orderEntity = deleteStmt.executeUpdate(deleteFromOrderEntity);
+            }
+        }
+    }
+    @SneakyThrows
+    public String getPaymentStatus() {
+        var status = "SELECT status FROM payment_entity ORDER BY created DESC LIMIT 1;";
+        return runner.query(conn, status, new ScalarHandler<>());
+    }
+    @SneakyThrows
+    public String getTransactionId() {
+        var transactionId = "SELECT transaction_id FROM payment_entity ORDER BY created DESC LIMIT 1;";
+        return runner.query(conn, transactionId, new ScalarHandler<>());
+    }
+    @SneakyThrows
+    public String getPaymentId() {
+        var paymentId = "SELECT payment_id FROM order ORDER BY created DESC LIMIT 1;";
+        return runner.query(conn, paymentId, new ScalarHandler<>());
     }
 }
